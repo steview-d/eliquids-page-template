d422116d633bc8a49db22f51988e44bf4a5254bc
Dave Laffan <dave.laffan@outlook.com>
2021-01-22 16:02:38 +0000

    Add feedback to copy code button

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,13 @@ $( document ).ready(function() {
     $('.copy').click(function() {
         foo = $(this).next().prop('outerHTML');
         navigator.clipboard.writeText(foo);
+
+        $(this).text("Copied!")
+        $this = $(this)
+        setTimeout(function(){
+            $this.text("Copy Code")
+        }, 1200);
     })
+
 });
 
